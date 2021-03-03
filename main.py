dfefbfddf63c087b9792f867a864fe2a36baa850
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2021-03-03 23:19:15 +0200

    All logic has been implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 class Location:
     def __init__(self, city, post_office):
         self.city = city
@@ -16,23 +17,23 @@ class Item:
 
 
 class Vehicle:
-    def __init__(self, vehicle_num, is_available):
+    def __init__(self, vehicle_num):
         self.vehicle_num = vehicle_num
-        self.is_available = is_available
+        self.is_available = True
 
 
 class Order:
-    def __init__(self, username, city, post_office, items):
+    def __init__(self, user_name, city, post_office, items):
         self.order_id = randint(1000, 5000)
-        self.username = username
+        self.user_name = user_name
         self.location = city, post_office
         self.items = items
+        self.total_price = sum([item.price for item in list(self.items)])
         self.vehicle = None
-        print(str(self))
+        print(self)
 
     def __str__(self):
-        total_price = sum([item.price for item in list(self.items)])
-        return f"Your order #{self.order_id} is sent to {self.location}. Total price: {total_price} UAH."
+        return f"Your order #{self.order_id} is sent to {self.location[0]}. Total price: {self.total_price} UAH."
 
 
 class LogisticSystem:
@@ -41,9 +42,15 @@ class LogisticSystem:
         self.vehicles = vehicles
 
     def place_order(self, order):
-        self.orders.append(order)
+        for vehicle in self.vehicles:
+            if vehicle.is_available:
+                vehicle.is_available = False
+                self.orders.append(order)
+                return None
+        return "There is no available vehicle to deliver an order."
 
     def track_order(self, order_id):
-
-
-
+        for order in self.orders:
+            if order.order_id == order_id:
+                return print(order)
+        return "No such order."
